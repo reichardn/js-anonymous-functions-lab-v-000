9a7579bdb1089bc312db130daedee6ccbb232277
reichardn <reichardn@gmail.com>
2016-08-04 00:29:41 -0400

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -14,31 +14,43 @@ function randomNumber() {
   return Math.floor(Math.random());
 }
 
+function setMenu(e) {
+  $('#menuOptions').html("You can play a song, pause a song, or view all the songs")
+}
+
 function menu() {
-  $('#menu').on('click', function(e) {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  $('#menu').on('click', setMenu);
+}
+
+function setSong(e) {
+  $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  return "now playing " + songToPlay + " by " + theArtist;
 }
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', setSong);
+}
+
+// -----------------------
+
+function pauseSong(e) {
+  $('#songPaused').html(songToPlay + " is paused");
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
+  $('#pause').on('click', pauseSong);
+}
+
+// -----------------------
+
+function showSong(e) {
+  var songs = "";
+  for (var key in jukeboxSongs) {
+    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+  }
+  $('#showSongs').html(songs);
 }
 
 function show() {
-  $('#show').on('click', function(e) {
-    var songs = "";
-    for (var key in jukeboxSongs) {
-      songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-    }
-    $('#showSongs').html(songs);
-  });
+  $('#show').on('click', showSong);
 }
